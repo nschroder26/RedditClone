8025fa85394eacafa9940d1f06b4e4412d4de1c8
Nicholas Schroder <nrs4492@hotmail.com>
2023-04-22 16:40:03 -0400

    add signup page, update navbar links to direct to homepage. add create account link on landing page form

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   get 'static_pages/home'
+  get 'static_pages/signup'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
