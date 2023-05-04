605ad9be81a366a5736ae655ebee391d89832179
Nicholas Schroder <nrs4492@hotmail.com>
2023-05-03 21:37:08 -0400

    revamp home page to actual landing page. revamp navbar with links to signup/signin. conditionally shows signout when user is logged in. removed original signup and login forms. updated routes. added conditional to only show alerts if one is generated

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
   root 'static_pages#home'
-  get 'static_pages/signup'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
