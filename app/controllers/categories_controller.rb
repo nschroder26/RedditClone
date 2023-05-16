3e6f59a736001d814b509562f30e2c5e2c307c1c
Nick Schroder <nrs4492@hotmail.com>
2023-05-16 16:01:56 -0400

    add category methods, add admin restriction for category creation, updating, and deleting. update route for categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,6 +1,39 @@
 class CategoriesController < ApplicationController
+  before_action :require_admin_role, only: [:new, :create, :edit, :update, :destroy]
 
   def index
     @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to @category, notice: "Category has been created"
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @category = Category.find(params[:id])
+  end
+
+  def update
+    @category = Category.find(params[:id])
+    if @category.update(category_params)
+      redirect_to @category, notice: "Category has been updated"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @category = Category.find(params[:id])
+    @category.destroy
+    redirect_to categories_path, notice: "Category has been deleted"
   end
 end
