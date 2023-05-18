8535debd12576edd792f7059ee55064c4bdb5895
Nick Schroder <nrs4492@hotmail.com>
2023-05-18 16:31:02 -0400

    additional formatting for category list and buttons. After deleting a category the name of deleted category is shown in notice

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -34,7 +34,7 @@ class CategoriesController < ApplicationController
   def destroy
     @category = Category.find(params[:id])
     @category.destroy
-    redirect_to categories_path, notice: "Category has been deleted"
+    redirect_to categories_path, notice: "#{@category.name} has been deleted"
   end
 
   private
